9f07f02e889fa2ec24c8165e3146027d60b37479
JaydenLee1116 <vv55adss@gmail.com>
2023-08-13 15:35:46 +0900

    ✨ feat: Node 자료구조 구현

---
+++ packages/library/src/Node/index.ts
@@ -0,0 +1,8 @@
+export class Node<T> {
+  public value: T;
+  public next: Node<T> | null;
+  constructor(value: T, next: Node<T> | null = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
